78c0fc0b1c31b8c1d989591bce5a3ada1d4c77fa
wassimelfen <wassimelfen@gmail.com>
2022-01-10 19:06:19 +0100

    authentication pictures - challenge requests

--- src/components/Challenges/FirstChallenge.jsx
+++ src/components/Challenges/FirstChallenge.jsx
@@ -1,34 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 import FirstExercice from "./FirstExercice.jsx";
 
 const FirstChallenge = () => {
-  const [fetchChallengeData, setfetchChallengeData] = useState({
-    quations: [],
-  });
 
-  useEffect(() => {
-    fetchChallenges();
-  });
-
-  const fetchChallenges = () => {
-    axios.get("http://localhost:8000/api/challenge/1").then(({ data }) => {
-      setfetchChallengeData(data);
-    });
-  };
-
-  // const oneQuiz = () => {
-  //   for(var i=0; i<fetchChallengeData.quations.length;i++){
-
-  //   }
-  // }
 
   return (
     <div>
-      <FirstExercice questions={fetchChallengeData.quations} />
-
-      <button>Next</button>
+      <FirstExercice />
     </div>
   );
 };
